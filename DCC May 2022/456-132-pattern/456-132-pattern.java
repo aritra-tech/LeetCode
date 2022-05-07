a16063737d436f7f45bbf258c84f340c6fe28edb
Aritra Das <80090908+aritra-tech@users.noreply.github.com>
2022-05-07 19:13:27 +0530

    Update 456-132-pattern.java

--- DCC May 2022/456-132-pattern/456-132-pattern.java
+++ DCC May 2022/456-132-pattern/456-132-pattern.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public boolean find132pattern(int[] nums) {
         Stack<Integer> st = new Stack<>();
@@ -6,6 +7,21 @@ class Solution {
             if(nums[i] < a) return true;
             while(!st.isEmpty() && st.peek() < nums[i]) a = st.pop();
             st.push(nums[i]);
+        }
+        return false;
+    }
+}
+
+Brute Force Solution
+
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                for(int k=j+1;k<nums.length;k++){
+                    if(nums[i] < nums[k] && nums[k] < nums[j]) return true;
+                }
+            }
         }
         return false;
     }
