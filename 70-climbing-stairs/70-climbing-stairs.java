2565114b919a52a6e3653bf7616924a1d909538a
Aritra Das <80090908+aritra-tech@users.noreply.github.com>
2022-05-11 17:22:14 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (5.25%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==0 || n==1 ||n==2) return n;
+        int[] a = new int[n];
+        a[0]=1;
+        a[1]=2;
+        for(int i=2;i<n;i++){
+            a[i] = a[i-1] + a[i-2];
+        }
+        return a[n-1];
+    }
+}
