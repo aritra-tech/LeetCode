ade95b19e660000a3fe6281f66a1fa654d4f8f15
Aritra Das <80090908+aritra-tech@users.noreply.github.com>
2022-06-29 11:00:35 +0530

    Time: 3 ms (99.34%), Space: 41.8 MB (99.29%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        int[] mappingDictStoT = new int[256];
+        Arrays.fill(mappingDictStoT, -1);
+        
+        int[] mappingDictTtoS = new int[256];
+        Arrays.fill(mappingDictTtoS, -1);
+        
+        for (int i = 0; i < s.length(); ++i) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            
+            // Case 1: No mapping exists in either of the dictionaries
+            if (mappingDictStoT[c1] == -1 && mappingDictTtoS[c2] == -1) {
+                mappingDictStoT[c1] = c2;
+                mappingDictTtoS[c2] = c1;
+            }
+            
+            // Case 2: Ether mapping doesn't exist in one of the dictionaries or Mapping exists and
+            // it doesn't match in either of the dictionaries or both 
+            else if (!(mappingDictStoT[c1] == c2 && mappingDictTtoS[c2] == c1)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
