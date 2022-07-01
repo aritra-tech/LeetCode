afa5e23cd1128073d368e2a06fa29e34c6838ad8
Aritra Das <aritrarick2002@gmail.com>
2022-07-02 02:10:39 +0530

    Question Added

---
+++ Dynamic Programming/139-word-break/139-word-break.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int len = s.length();
+        boolean[] f = new boolean[len+1];
+        f[0] = true;
+        for (int i=1; i<len+1; i++)
+            for (int j=0; j<i; j++)
+                if (f[j] && wordDict.contains(s.substring(j,i)))
+                {
+                    f[i] = true;
+                    break;
+                }
+        return f[len];
+    }
+}
