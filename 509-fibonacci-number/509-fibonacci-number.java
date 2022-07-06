0c7fcf2abdb57f68d5c4c91a59b6da1f0df41296
Aritra Das <80090908+aritra-tech@users.noreply.github.com>
2022-07-06 10:43:35 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (57.51%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,14 +1,12 @@
-class Solution 
-{
-    int[] fib_cache = new int[31];
-	
-	public int fib(int N)
-    {
-        if(N <= 1)
-            return N;
-        else if(fib_cache[N] != 0)
-            return fib_cache[N];
-		else 
-            return fib_cache[N] = fib(N - 1) + fib(N - 2);
+class Solution {
+    public int fib(int n) {
+        if (n < 2) return n;
+        int a = 0, b = 1, temp;
+        for (int i = 1; i < n; i++) {
+            temp = a;
+            a = b;
+            b += temp;
+        }
+        return b;
     }
 }
